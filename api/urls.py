03576c1a1d6629410ec3af5470730c288bfd5aea
Ephraim <ephraimakolo2017@gmail.com>
2026-09-02 17:21:30 +0100

    remove redundant api

--- api/urls.py
+++ api/urls.py
@@ -6,8 +6,8 @@ from api.views import SampleDataView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/sample-data/", SampleDataView.as_view(), name="sample-data"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("sample-data/", SampleDataView.as_view(), name="sample-data"),
 ]
 
