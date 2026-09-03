016b62c9e3fd16207ebcee56bfa3b8e59c1cb043
Ephraim <ephraimakolo2017@gmail.com>
2026-09-03 05:28:25 +0100

    add admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Profile, Task
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
+    list_filter = ('role',)
+    search_fields = ('user__username',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'status', 'created_at', 'updated_at')
+    list_filter = ('status',)
+    search_fields = ('title', 'owner__username')
